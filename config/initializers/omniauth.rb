3ee20f876bc02385701135824a01f9b56e9d932a
jessica <jessicazhuge1@gmail.com>
2016-10-18 22:59:48 +0000

    Changed facebook dev

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,16 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 #previous app?
-#   provider :facebook, '940146309424515', 'b78f11ced7744092c8a8c59164749f41', {:info_fields => 'email,first_name,last_name,location,gender', :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}} 
+  provider :facebook, '940146309424515', 'b78f11ced7744092c8a8c59164749f41', {:info_fields => 'email,first_name,last_name,location,gender', :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}} 
 
-#currently, this is the ID and AS for the test application 
-provider :facebook, '165788530545701', '42740cec0bcc849919de16d105e714f7', {:info_fields => 'email,first_name,last_name,location,gender', :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}} 
+# provider :facebook, '165763490548205', 'ddf2723786c1f581c7a28a9dd4095ac9', {:info_fields => 'email,first_name,last_name,location,gender', :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}} 
+
+
+# #currently, this is the ID and AS for the test application 
+# provider :facebook, '165788530545701', '42740cec0bcc849919de16d105e714f7', {:info_fields => 'email,first_name,last_name,location,gender', :client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}} 
+
+
+
+
 
 end
