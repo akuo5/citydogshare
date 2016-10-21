a8b5cf8a8dee6bef496f430c480033c1ced711f1
jessica <jessicazhuge1@gmail.com>
2016-10-21 01:55:36 +0000

    fb-auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,6 +34,7 @@ class SessionsController < ApplicationController
       redirect_to root_path()
     else
       @new_user = User.create()
+      byebug if @new_user == nil
       @new_user.update_credentials(params[:auth][:credentials])
       @new_user.facebook_info_update(params[:auth])
       redirect_to create_session_path(:user => @new_user)
