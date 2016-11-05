b21325c9d83d43016781610e6957e7680ae02461
Jacen Sherman <jacen@berkeley.edu>
2016-11-05 19:37:10 +0000

    Jacen - Fix Heroku assets pipeline attempt 1

--- config/application.rb
+++ config/application.rb
@@ -55,9 +55,6 @@ module Citydogshare
     
     # Enable the asset pipeline
     config.assets.enabled = true
-    
-    # Enable static assets
-    config.serve_static_assets = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
