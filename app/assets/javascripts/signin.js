363718438a9b3a52c5f41190821cfad931c844c6
jessica <jessicazhuge1@gmail.com>
2016-11-17 05:07:46 +0000

    added the js code

---
+++ app/assets/javascripts/signin.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+    
+    $('#signup-form').hide(0); 
+       
+       
+    $("#sign-up-btn").click(function(){
+        $("#signup-form").show();
+    });
+        
+           
+    $("#close-link").click(function(){
+        $("#signup-form").hide();
+    });
+
+  }); // end of document ready
+ 
+ 
+})(jQuery); // end of jQuery name space
