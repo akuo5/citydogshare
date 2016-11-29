4864dab9ebd6db6d8e11b9e67400fdd549dc6843
jessica <jessicazhuge1@gmail.com>
2016-11-29 11:25:32 +0000

    precompile

---
+++ public/assets/signin-2bcafa180ef0711f5f963aa09c4da0f0229fe4e100cffabbd61bf88fa70804f2.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+    
+    $('#signup-form').hide(0); 
+       
+       
+    $("#sign-up-btn").click(function(){
+        $("#signup-form").show();
+    });
+        
+           
+    $("#close-link").click(function(){
+        $("#signup-form").hide();
+    });
+    
+  });
+ 
+})(jQuery); // end of jQuery name space
+;
