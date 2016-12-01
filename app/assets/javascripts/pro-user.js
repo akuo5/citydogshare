210c65fe4623f7c5ea9d1bd08b6293c519c1027f
jessica <jessicazhuge1@gmail.com>
2016-12-01 17:22:28 +0000

    Added precompiled assets

---
+++ app/assets/javascripts/pro-user.js
@@ -0,0 +1,46 @@
+(function($){
+  $(document).on('ready page:load', function(){
+    
+    $("#pro-cal-button").hide(0);
+    
+    // if (proUser) {
+    //   $("#pro-cal-button").show();
+    //   $("#checkboxid").prop('checked', true);
+    // }
+    
+    $("#pro-toggle").click(function () {
+      
+    var tog_url = "/users/" + userId + "/toggle";
+    
+    var check = $("#pro-toggle").is(":checked");
+    if(check) {
+        //use ajax to change to pro user. show the button for my calendar 
+        $("#pro-cal-button").show();
+        
+    } else {
+        //use ajax to change pro to non user. make button disappear
+        $("#pro-cal-button").hide(0);
+    }
+    
+    // $.post(tog_url);
+    
+      // $.ajax({
+      //     type: "POST",
+      //     url: tog_url,
+      //     success: function (msg) {
+      //       alert('Success');
+      //       if (msg != 'success') {
+      //           alert('Fail');
+      //       }
+      //     },
+      //     error: function(response) {
+      //           console.log(response.status + " " + response.statusText);
+      //     }
+      //     });
+    
+  
+      });
+  });
+ 
+})(jQuery); // end of jQuery name space
+
