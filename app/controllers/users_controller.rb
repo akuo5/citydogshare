8bf3c83d7b257326750e5eca95b85fb3dd7b7c03
jessica <jessicazhuge1@gmail.com>
2016-12-01 07:09:52 +0000

    changed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,6 +75,7 @@ class UsersController < ApplicationController
   
   def toggle_pro
     state = params[:val] #this is the current state of the user's pro status: true or false
+    puts("HERLLLOOOOOO")
     if @current_user.is_pro? and state == 'true'
       @current_user.set_pro(false)
       # respond with false
